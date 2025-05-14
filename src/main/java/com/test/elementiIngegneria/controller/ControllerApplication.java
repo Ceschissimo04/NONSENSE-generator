6ccbd0a043b7ec325e9bb3a12ceab98cb9a09d5e
alesalaa <ale.salamone10@gmail.com>
2025-05-14 23:41:11 +0200

    Form corretto e collegamento con controller

--- src/main/java/com/test/elementiIngegneria/controller/ControllerApplication.java
+++ src/main/java/com/test/elementiIngegneria/controller/ControllerApplication.java
@@ -2,13 +2,34 @@ package com.test.elementiIngegneria.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class ControllerApplication {
 
     @GetMapping("/")
-    public String Controller(){
+    public String Controller() {
         return "index";
     }
-    
+
+    //pagina a cui rimanda il form
+    @PostMapping("/generate")
+    public String generaNonsense(
+            @RequestParam("sentence") String sentence,
+            @RequestParam("template") String template,
+            @RequestParam("tense") String tense,
+            Model model) {
+
+        String nonsense = "Sentence: " + sentence + " ;  Template: " + template + " ; Tense: " + tense;
+
+        // passa i risultati alla pagina index.html
+        model.addAttribute("nonsenseResult", nonsense);
+        model.addAttribute("inputSentence", sentence);
+        model.addAttribute("selectedTemplate", template);
+        model.addAttribute("selectedTense", tense);
+
+        return "index";
+    }
 }
