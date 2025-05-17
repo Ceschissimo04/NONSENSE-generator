8df4f0c5864ecdddb34f8444b0f2ef3bd5eb9c7d
alesalaa <ale.salamone10@gmail.com>
2025-05-17 11:50:26 +0200

    Finita classe Verbs e qualche aggiustamento
    Controllate ma dovrebbe essere così che intendevate gestire la classe Verbs.
    Ho sostituito il metodo di caricamento da file senza usare iterator che forse erano più complicati, decidete voi quale tenere

--- src/main/java/com/test/elementiIngegneria/controller/ControllerApplication.java
+++ src/main/java/com/test/elementiIngegneria/controller/ControllerApplication.java
@@ -3,10 +3,6 @@ package com.test.elementiIngegneria.controller;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
@@ -16,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.test.elementiIngegneria.model.Nouns;
 import com.test.elementiIngegneria.model.Verbs;
 
 import org.springframework.ui.Model;
@@ -27,12 +22,15 @@ public class ControllerApplication {
     @GetMapping("/")
     public String Controller(Model model) {
 
-        Verbs nouns = new Verbs();
+        Verbs verbs = new Verbs();
 
         System.out.println();
 
-        try {model.addAttribute("templateList", loadTemplate("src/main/resources/static/templates/templates.txt"));}
-        catch (IOException e) {e.printStackTrace();}
+        try {
+            model.addAttribute("templateList", loadTemplate("src/main/resources/static/templates/templates.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         String apiKey = loadApiKey("config.json");
 
@@ -41,63 +39,8 @@ public class ControllerApplication {
             return "";
         }
 
-        String text = "the pen was on the table.";
-        String language = "en";
-
-        // JSON della richiesta
-        String jsonInput = String.format(
-                "{\n" +
-                        "  \"document\": {\n" +
-                        "    \"type\": \"PLAIN_TEXT\",\n" +
-                        "    \"language\": \"%s\",\n" +
-                        "    \"content\": \"%s\"\n" +
-                        "  },\n" +
-                        "  \"encodingType\": \"UTF16\"\n" +
-                        "}",
-                language, text);
-
         return "index";
     }
-    /*
-     * try {
-     * 
-     * @SuppressWarnings("deprecation")
-     * URL url = new
-     * URL("https://language.googleapis.com/v1/documents:analyzeSyntax?key=" +
-     * apiKey);
-     * HttpURLConnection con = (HttpURLConnection) url.openConnection();
-     * con.setRequestMethod("POST");
-     * con.setDoOutput(true);
-     * con.setRequestProperty("Content-Type", "application/json; utf-8");
-     * con.setRequestProperty("Accept", "application/json");
-     * 
-     * // Scrivi JSON nel body
-     * try (OutputStream os = con.getOutputStream()) {
-     * byte[] input = jsonInput.getBytes(java.nio.charset.StandardCharsets.UTF_8);
-     * os.write(input, 0, input.length);
-     * }
-     * 
-     * // Ricevi risposta
-     * int responseCode = con.getResponseCode();
-     * System.out.println("Codice risposta: " + responseCode);
-     * 
-     * try (BufferedReader br = new BufferedReader(
-     * new InputStreamReader(con.getInputStream(),
-     * java.nio.charset.StandardCharsets.UTF_8))) {
-     * StringBuilder response = new StringBuilder();
-     * String line;
-     * 
-     * while ((line = br.readLine()) != null) {
-     * response.append(line.trim());
-     * }
-     * 
-     * System.out.println("Risposta JSON:");
-     * System.out.println(response.toString());
-     * }
-     * } catch (IOException e) {
-     * System.err.println("Errore durante la richiesta HTTP: " + e.getMessage());
-     * }
-     */
 
     public static String loadApiKey(String filePath) {
         try (FileReader reader = new FileReader(filePath)) {
@@ -125,13 +68,20 @@ public class ControllerApplication {
         model.addAttribute("selectedTemplate", template);
         model.addAttribute("selectedTense", tense);
 
+        try {
+            model.addAttribute("templateList", loadTemplate("src/main/resources/static/templates/templates.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return "index";
     }
 
     private ArrayList<String> loadTemplate(String path) throws IOException {
         ArrayList<String> templateList = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(path));
-        while (br.ready()) templateList.add(br.readLine());
+        while (br.ready())
+            templateList.add(br.readLine());
         br.close();
         return templateList;
     }
--- src/main/java/com/test/elementiIngegneria/model/Verbs.java
+++ src/main/java/com/test/elementiIngegneria/model/Verbs.java
@@ -1,57 +1,100 @@
 package com.test.elementiIngegneria.model;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class Verbs implements Word {
 
     private HashMap<String, String[]> allVerbs;
+    private ArrayList<String> keys;
 
     public Verbs() {
         allVerbs = new HashMap<>();
+        keys = new ArrayList<>();
         getFromFile();
     }
 
+    //
     @Override
     public String getRandom(String optional) {
-        int intRandom = (int) (Math.floor(Math.random() * allVerbs.size()));
-        //qua ci andrebbe un altro iterator per prendere un verbo a caso perche l'hash-map non ha un get(int) per semplificare questo, nel while con l'iterator dovrei anche aggiungere un int che mi tenga conto della posizione
-        return "";
+        int intRandom = (int) (Math.floor(Math.random() * keys.size()));
+        String key = keys.get(intRandom);
+
+        String[] values = allVerbs.get(key);
+        switch (optional) {
+            case "presente":
+                return values[0];
+            case "passato":
+                return values[1];
+            case "futuro":
+                return values[2];
+            default:
+                return key;
+        }
     }
 
+    // Usando questo metodo non dovrebbe essere necessario getStringJson
     @Override
     public void getFromFile() {
         try (FileReader reader = new FileReader("src/main/resources/static/files/verbs.json")) {
             JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
-            Iterator<String> it = json.keySet().iterator();
-            while (it.hasNext()){
-                String key = it.next();
-                allVerbs.put(key, getStringJson(key,json));
+            for (String key : json.keySet()) {
+                JsonArray arr = json.getAsJsonArray(key);
+                String[] values = new String[arr.size()];
+                for (int i = 0; i < arr.size(); i++) {
+                    values[i] = arr.get(i).getAsString();
+                }
+                allVerbs.put(key, values);
+                keys.add(key);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    //secondo iterator perche senno non si riesce a fare un hash-map, questa colpa di json 
-    private String[] getStringJson(String key, JsonObject json) {
-        Iterator<JsonElement> strings = json.get(key).getAsJsonArray().iterator();
-        String[] array = new String[3];
-        int index = 0;
-        while (strings.hasNext()) {
-            array[index] = strings.next().toString();
-            index++;
-        }
-        return array;
+    /*
+     * @Override
+     * public void getFromFile() {
+     * try (FileReader reader = new
+     * FileReader("src/main/resources/static/files/verbs.json")) {
+     * JsonObject json = JsonParser.parseReader(reader).getAsJsonObject();
+     * Iterator<String> it = json.keySet().iterator();
+     * while (it.hasNext()) {
+     * String key = it.next();
+     * allVerbs.put(key, getStringJson(key, json));
+     * }
+     * } catch (IOException e) {
+     * e.printStackTrace();
+     * }
+     * }
+     * 
+     * // secondo iterator perche senno non si riesce a fare un hash-map, questa
+     * colpa
+     * // di json
+     * private String[] getStringJson(String key, JsonObject json) {
+     * Iterator<JsonElement> strings = json.get(key).getAsJsonArray().iterator();
+     * String[] array = new String[3];
+     * int index = 0;
+     * while (strings.hasNext()) {
+     * array[index] = strings.next().toString();
+     * index++;
+     * }
+     * return array;
+     * }
+     */
+
+    public HashMap<String, String[]> getAllVerbs() {
+        return allVerbs;
+    }
+
+    public ArrayList<String> getKeys() {
+        return keys;
     }
 
 }
