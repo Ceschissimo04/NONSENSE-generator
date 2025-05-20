c3680a2650ed56e4ae64622dbd34b39eb8d9de94
AlfonsoBuonocore <buonocore.a2004@gmail.com>
2025-05-20 12:00:43 +0200

    css syntaxt tree sistemato

--- src/main/java/com/test/elementiIngegneria/controller/ControllerApplication.java
+++ src/main/java/com/test/elementiIngegneria/controller/ControllerApplication.java
@@ -11,12 +11,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.test.elementiIngegneria.model.Node;
 
 @Controller
 public class ControllerApplication {
     public static final String DEFAULT_LANGUAGE = "en";
-
+    
     @GetMapping("/")
     public String Controller(Model model) {
         try {
